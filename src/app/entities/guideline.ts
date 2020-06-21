06a28c7a45ff6351f05eeb046094aa5e70377d0d
Ashley Sheridan <ash@ashleysheridan.co.uk>
2020-06-21 22:51:19 +0100

    fleshing out the guidelines with more details

--- src/app/entities/guideline.ts
+++ src/app/entities/guideline.ts
@@ -8,13 +8,23 @@ export class Guideline {
 	shortDescription: string;
 	longDescription: string;
 	testAgainst: ContentType[];
+	jsSnippets: string[];
 	
-	constructor(level: GuidelineLevel, guidelineNumber: string, guidelineName: string, shortDescription: string, longDescription: string, testAgainst: ContentType[]) {
+	constructor(
+		level: GuidelineLevel, 
+		guidelineNumber: string,
+		guidelineName: string, 
+		shortDescription: string, 
+		longDescription: string, 
+		testAgainst: ContentType[],
+		jsSnippets: string[]
+	) {
 		this.level = level;
 		this.guidelineNumber = guidelineNumber;
 		this.guidelineName = guidelineName;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 		this.testAgainst = testAgainst;
+		this.jsSnippets = jsSnippets;
 	}
 }
--- src/app/repositories/all-guidelines.ts
+++ src/app/repositories/all-guidelines.ts
@@ -12,9 +12,18 @@ export class AllGuidelines {
 			GuidelineLevel.A,
 			'1.1.1',
 			'Text Alternatives',
-			'Provide text alternatives for any non-text content so that it can be changed into other forms people need, such as large print, braille, speech, symbols or simpler language.',
-			`long description`,
-			[ContentType.Audio, ContentType.Images, ContentType.Video]
+			'Provide text alternatives for any non-text content.',
+			`<p>All non-text content (e.g. images, videos, graphs, etc) should have a text alternative. Check that:</p>
+			<ul>
+				<li>Images have <code>alt</code> text</li>
+				<li>Form controls have labels</li>
+				<li>Videos and audio clips have text alternatives</li>
+				<li>Decorative/purely visual elements are hidden from assisstive tech.</li>
+			</ul>`,
+			[ContentType.Audio, ContentType.Images, ContentType.Video, ContentType.Forms],
+			[
+				'console.log(document.querySelectorAll("img:not([alt])"))'
+			]
 		),
 		new Guideline(
 			GuidelineLevel.AA,
@@ -22,7 +31,8 @@ export class AllGuidelines {
 			'Captions (Live)',
 			'Captions are provided for all live audio content in synchronized media.',
 			'long description',
-			[ContentType.Audio, ContentType.Video]
+			[ContentType.Audio, ContentType.Video],
+			[]
 		),
 	];
 }
