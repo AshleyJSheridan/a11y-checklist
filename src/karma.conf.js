6b067c5d8664e24951794a8b2674a78f4f65cae9
Ashley Sheridan <ash@ashleysheridan.co.uk>
2020-12-08 10:53:18 +0000

    switchout out Chrome for a headless Fx for testing as Chrome doesn't want to play nice on my Win10 installation

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,7 +25,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['FirefoxHeadless'],
+	customLaunchers: {
+		'FirefoxHeadless': {
+			base: 'Firefox',
+			flags: [
+				'-headless'
+			]
+		}
+	},
     singleRun: false,
     restartOnFileChange: true
   });
